b0d204df9cf2d9d4e759f645ff9194d895f8f28d
Lucy Linder <lucy.derlin@gmail.com>
2017-05-14 17:08:58 +0200

    add comment to load-data script

--- spark-shell-scripts/load-data-example.scala
+++ spark-shell-scripts/load-data-example.scala
@@ -4,14 +4,32 @@
   * @author Lucy Linder <lucy.derlin@gmail.com>
   */
 
-// before use, set:
+// this script can be used in spark-shell. First, launch the shell with dependencies:
+// ```
+// spark-shell --jars target/scala-2.11/bda-project-lsa-assembly-1.0.jar
+// ```
+// Then, type the following:
+// ```
+// val path = "wikidump.xml"
+// val stopwordsFile = "src/main/resources/stopwords.txt"
+// val numTerms = 2000
+// val k = 100
+//
+// :load spark-shell-scripts/load-data-example.scala
+// ```
+// you will then have the following variables at your disposal:
+// * `docTermMatrix`: a DataFrame `[title: string, tfidfVec: vector]`
+// * `termIds`: an array of words. Indexes are used in the `tfidfVec` vector of `docTermMatrix`
+// * `docIds`: a map `Long => String` mapping a document id with its title
+// * `termIdfs`: a array of Double corresponding to the relative frequency of a word in the corpus
+
+
 /*
  val path = "wikidump.xml"
  val stopwordsFile = "src/main/resources/stopwords.txt"
  val numTerms = 2000
  val k = 100
   */
-
 
 import bda.lsa.AssembleDocumentTermMatrix
 import org.apache.spark.sql.Dataset
