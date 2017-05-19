650370e0ce515c258925e16a5d55790d902a1398
Lucy Linder <lucy.derlin@gmail.com>
2017-05-19 20:40:51 +0200

    use proper serialization of SVD model (HDFS support)

--- src/main/scala/bda/lsa/svd/RunSVD.scala
+++ src/main/scala/bda/lsa/svd/RunSVD.scala
@@ -3,7 +3,7 @@ package bda.lsa.svd
 import bda.lsa.{baseDir, docTermMatrixToCorpusRDD, getData}
 import org.apache.spark.SparkContext
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
-import org.apache.spark.mllib.linalg.{Matrix, SingularValueDecomposition, Vector => MLLIBVector}
+import org.apache.spark.mllib.linalg.{Matrix, SingularValueDecomposition => MLLIB_SingularValueDecomposition, Vector => MLLIBVector, Vectors => MLLIBVectors}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.sql.{DataFrame, SparkSession}
@@ -19,7 +19,6 @@ object RunSVD {
   def main(args: Array[String]): Unit = {
     // parse arguments
     val k = if (args.length > 0) args(0).toInt else 100
-    val pathSuffix = if (args.length > 1) args(1) else pathSuffix
 
     val spark = SparkSession.builder().
       config("spark.serializer", classOf[KryoSerializer].getName).
@@ -33,54 +32,53 @@ object RunSVD {
     vecRDD.cache()
 
     val mat = new RowMatrix(vecRDD)
-    val model: SingularValueDecomposition[RowMatrix, Matrix] = mat.computeSVD(k, computeU = true)
+    val model: MLLIB_SingularValueDecomposition[RowMatrix, Matrix] = mat.computeSVD(k, computeU = true)
 
     //  val U: RowMatrix = model.U // The U factor is a RowMatrix.
     //  val s: MLLIBVector = model.s // The singular values are stored in a local dense vector.
     //  val V: Matrix = model.V // The V factor is a local dense matrix.
 
     // save all
-    saveModel(model)
+    saveModel(spark, model)
   }
 
   //--------------------------------
 
   /**
-    * Save the model to disk
-    * @param model
-    * @return
+    * Save the model to disk (either locally or on hdfs depending on the spark mode)
+    *
+    * @param spark the current spark session
+    * @param model  the model to save
     */
-  def saveModel(model: SingularValueDecomposition[RowMatrix, Matrix]) = {
+  def saveModel(spark: SparkSession, model: MLLIB_SingularValueDecomposition[RowMatrix, Matrix]) : Unit = {
     // U is distributed => use RDD utils
     model.U.rows.saveAsObjectFile(baseDir + pathSuffix + "/U")
 
-    // s is a local vector => serialize
-    new java.io.ObjectOutputStream(new java.io.FileOutputStream(baseDir + pathSuffix + "/s")) {
-      writeObject(model.s)
-      close()
-    }
-    // V is a local Matrix => serialize
-    new java.io.ObjectOutputStream(new java.io.FileOutputStream(baseDir + pathSuffix + "/V")) {
-      writeObject(model.V)
-      close()
-    }
+    // s is a local vector => parallelize so it is also saved to hdfs in cluster mode
+    spark.sparkContext.parallelize(model.s.toArray).saveAsObjectFile(baseDir + pathSuffix + "/s")
+
+    // s is a local Matrix => parallelize so it is also saved to hdfs in cluster mode
+    spark.sparkContext.parallelize(model.V.rowIter.toSeq).saveAsObjectFile(baseDir + pathSuffix + "/V")
   }
 
-  def loadModel(spark: SparkSession): SingularValueDecomposition[RowMatrix, Matrix] = {
+  /**
+    * Load a model previously saved by [[saveModel()]].
+    * @param spark the current spark session
+    * @return the SVD model
+    */
+  def loadModel(spark: SparkSession): MLLIB_SingularValueDecomposition[RowMatrix, Matrix] = {
 
-    val U: RowMatrix = new RowMatrix(spark.sparkContext.objectFile(baseDir + "svd/U").asInstanceOf[RDD[MLLIBVector]])
+    val U: RowMatrix = new RowMatrix(spark.sparkContext.objectFile(baseDir + pathSuffix + "/U").asInstanceOf[RDD[MLLIBVector]])
 
-    val s: MLLIBVector = new java.io.ObjectInputStream(new java.io.FileInputStream(baseDir + "/svd/s")) {
-      val s = readObject().asInstanceOf[MLLIBVector]
-      close()
-    }.s
+    val s: MLLIBVector = MLLIBVectors.dense(spark.sparkContext.objectFile[Double](baseDir + pathSuffix + "/s").collect)
 
-    val V: Matrix = new java.io.ObjectInputStream(new java.io.FileInputStream(baseDir + "/svd/V")) {
-      val V = readObject().asInstanceOf[Matrix]
-      close()
-    }.V
+    // the matrix is more complex to recreate...
+    val vs : Array[MLLIBVector] = spark.sparkContext.objectFile[MLLIBVector](baseDir + pathSuffix + "/V").collect
+    val numRows = vs.length
+    val numCols = vs(0).toArray.length
+    val V: Matrix = org.apache.spark.mllib.linalg.Matrices.dense(numRows, numCols, vs.map(_.toArray).flatten)
 
-    new SingularValueDecomposition[RowMatrix, Matrix](U, s, V)
+    new MLLIB_SingularValueDecomposition[RowMatrix, Matrix](U, s, V)
   }
 
 }
