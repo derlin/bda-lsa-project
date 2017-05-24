3ed8599e07442c2e3367c69f16f5bc98d8c61bc1
Kewin Dousse <me@protectator.ch>
2017-05-24 10:17:33 +0200

    Optimizer uses custom values of alpha and beta, also changeable using arguments

--- src/main/scala/bda/lsa/lda/mllib/RunLDA.scala
+++ src/main/scala/bda/lsa/lda/mllib/RunLDA.scala
@@ -2,7 +2,10 @@ package bda.lsa.lda.mllib
 
 import bda.lsa._
 import org.apache.spark.ml.linalg.{Vector => ml_Vector}
-import org.apache.spark.mllib.clustering.{DistributedLDAModel => mllib_DistributedLDAModel, LDA => mllib_LDA}
+import org.apache.spark.mllib.clustering.{DistributedLDAModel => mllib_DistributedLDAModel,
+  LDA => mllib_LDA,
+  EMLDAOptimizer => mllib_EMLDAOptimizer,
+  OnlineLDAOptimizer => mllib_OnlineLDAOptimizer}
 import org.apache.spark.mllib.linalg.{Vector => mllib_Vector}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.serializer.KryoSerializer
@@ -19,18 +22,33 @@ object RunLDA {
   def main(args: Array[String]): Unit = {
     // parse arguments
     val k = if (args.length > 0) args(0).toInt else 100
+    val maxIterations = if (args.length > 1) args(1).toInt else 20
+    val alpha = if (args.length > 2) args(2).toInt else 1 // Typical value is (50 / nbTopics)
+    val beta = if (args.length > 3) args(3).toInt else 0.1 // Typical value is either 0.1 or (200 / vocabularySize)
+    val optimizerAlgorithm = if (args.length > 4) args(4).toLowerCase else "em" // Either "em" or "online"
+
+    val optimizer = optimizerAlgorithm.toLowerCase match {
+      case "em" => new mllib_EMLDAOptimizer
+      case "online" => new mllib_OnlineLDAOptimizer()
+      case _ => throw new IllegalArgumentException( s"Only algorithms 'em', 'online' are supported, not ${optimizerAlgorithm}.")
+    }
 
     val spark = SparkSession.builder().
       config("spark.serializer", classOf[KryoSerializer].getName).
       getOrCreate()
-    
+
     val (docTermMatrix, termIds, docIds, idfs): (DataFrame, Array[String], Map[Long, String], Array[Double]) = getData(spark)
     val corpus: RDD[(Long, (mllib_Vector, String))] = docTermMatrixToCorpusRDD(spark, docTermMatrix)
 
     corpus.cache()
 
+    val LDA = new mllib_LDA()
+
     val model: mllib_DistributedLDAModel =
       new mllib_LDA().
+        setOptimizer(optimizer).
+        setAlpha(alpha).
+        setBeta(beta).
         setK(k).
         run(corpus.mapValues(_._1)).
         asInstanceOf[mllib_DistributedLDAModel]
