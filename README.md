90cacd1b6d8c95978ed224903607a4c4efd7ed94
Lucy Linder <lucy.derlin@gmail.com>
2017-05-28 15:31:04 +0200

    update readme + add svd querier

--- README.md
+++ README.md
@@ -1,4 +1,84 @@
+# Building and Running
+
+__Building the jar__
+
+This project uses sbt. To create the jar, use:
+    
+    export JAVA_OPTS="-Xms256m -Xmx4g"
+    sbt assembly
+
+The jar is now available under `target/scala-2.11/bda-project-lsa-assembly-1.0.jar`.
+
+__Project structure__
+
+The project is made of multiple spark programs. Each program stores its output on disk, the actual location depending on the properties set in `config.properties`.
+ 
+ A usual pipeline is:
+ 
+ 1. convert wikidump XML into plain text (`bda.lsa.preprocessing.XmlToParquetWriter`)
+ 2. create the vocabulary and the TF-IDF matrix (`bda.lsa.preprocessing.DocTermMatrixWriter`)
+ 3. create one of the models (`bda.lsa.svd.RunSVD`, `bda.lsa.lda.mllib.RunLDA`, `bda.lsa.lda.ml.RunLDA`)
+ 
+ At this point, the model is persisted somewhere and you can load it inside a spark-shell to interact with it. The classes `bda.lsa.svd.SVDQueryEngine`, `bda.lsa.lda.mllib.LDAQueryEngine` and `bda.lsa.lda.ml.LDAQueryEngine` implement useful queries to analyse the models. 
+
+
+__Example of pipeline__
+
+1. ensure you have a _wikidump_ somewhere to process.
+2. create the jar: 
+
+        export JAVA_OPTS="-Xms256m -Xmx4g"
+        sbt assembly
+        
+3. create a `config.properties` and add the following:
+
+        path.wikidump=wikidump-1500.xml # your xml 
+        path.base=/tmp/spark-wiki/   # a base path to store the results
+        
+4. convert XML to text:  
+ 
+        spark-submit --class bda.lsa.preprocessing.XmlToParquetWriter \
+            target/scala-2.11/bda-project-lsa-assembly-1.0.jar
+            
+    This will save the DataFrame `[(title: String, content: String) ]` in `path.base/wikidump-parquet`.
+    __important__: the job will fail if the aforementioned directory already exists !
+
+5. create the TF-IDF matrix:
+
+        spark-submit --class bda.lsa.preprocessing.DocTermMatrixWriter \
+                target/scala-2.11/bda-project-lsa-assembly-1.0.jar  \
+                <numTerms> [<percent> <numDocs>]
+                
+   The only required argument is `numTerms`: this is the size of the vocabulary.
+    
+6. Run one of the models. For example, for svd:
+    
+        spark-submit --class bda.lsa.svd.RunSVD \
+                       target/scala-2.11/bda-project-lsa-assembly-1.0.jar  \
+                       <k: default 100>
+     
+   The parameter `k` is the number of topics to infer, default to 100.
+   
+7. open a spark-shell and load the model:
+
+        spark-shell --jars target/scala-2.11/bda-project-lsa-assembly-1.0.jar 
+        val data = bda.lsa.getData(spark)
+        val model = bda.lsa.svd.RunSVD.loadModel(spark)
+        
+8. Optionally, use the querier in the shell:
+
+        val q = new bda.lsa.svd.SVDQueryEngine(spark, data)
+   
+   
+   
+# Models available
+
+TODO
+
+
 # Running on the daplab
+
+TODO
 
 export environment variables:
 
@@ -13,6 +93,7 @@ launch the shell on yarn:
     
     spark-shell --master yarn-client --num-executors 10 --driver-memory 20G --executor-memory 10g --conf spark.rpc.message.maxSize=100 spark.shuffle.manager=SORT spark.yarn.executor.memoryOverhead=4096 --jars target/scala-2.11/bda-project-lsa-assembly-1.0.jar
     
+    spark-shell --master yarn-client --num-executors 10 --driver-memory 20G --executor-memory 10g --conf spark.rpc.message.maxSize=100 spark.shuffle.manager=SORT spark.yarn.executor.memoryOverhead=4096 --jars target/scala-2.11/bda-project-lsa-assembly-1.0.jar
     
     
 Local
@@ -20,4 +101,9 @@ Local
     spark-submit --class bda.lsa.PrepareData --master "local[*]"  target/scala-2.11/bda-project-lsa-assembly-1.0.jar wikidump.xml wikidump.txt
     
     
-    spark-submit --class bda.lsa.PrepareData  --master yarn-client --num-executors 10 --driver-memory 20G --executor-memory 10g target/scala-2.11/bda-project-lsa-assembly-1.0.jar 
+    spark-submit --class bda.lsa.preprocessing.DocTermMatrixWriter  --master yarn-client --num-executors 3 --driver-memory 20G --executor-memory 10g target/scala-2.11/bda-project-lsa-assembly-1.0.jar 5000
+    
+    
+sbt out of memory error: 
+
+       export JAVA_OPTS="-Xms256m -Xmx4g"
---
+++ src/main/scala/bda/lsa/svd/SVDQueryEngine.scala
@@ -0,0 +1,173 @@
+package bda.lsa.svd
+
+
+import bda.lsa.Data
+import bda.lsa.preprocessing.AssembleDocumentTermMatrix
+import breeze.linalg.{DenseMatrix => BDenseMatrix, SparseVector => BSparseVector}
+import org.apache.spark.mllib.linalg.{Matrices, Matrix, SingularValueDecomposition, Vectors, Vector => MLLibVector}
+import org.apache.spark.ml.linalg.{Vector => MLVector}
+import org.apache.spark.mllib.linalg.distributed.RowMatrix
+import org.apache.spark.serializer.KryoSerializer
+import org.apache.spark.sql.{Dataset, SparkSession}
+
+import scala.collection.Map
+import scala.collection.mutable.ArrayBuffer
+
+/**
+  * date: 28.05.17
+  *
+  * @author Lucy Linder <lucy.derlin@gmail.com>
+  */
+class SVDQueryEngine(val model: SingularValueDecomposition[RowMatrix, Matrix], val data: Data) {
+
+
+  val VS: BDenseMatrix[Double] = multiplyByDiagonalMatrix(model.V, model.s)
+  val normalizedVS: BDenseMatrix[Double] = rowsNormalized(VS)
+  val US: RowMatrix = multiplyByDiagonalRowMatrix(model.U, model.s)
+  val normalizedUS: RowMatrix = distributedRowsNormalized(US)
+
+  val idTerms: Map[String, Int] = data.termIds.zipWithIndex.toMap
+  val idDocs: Map[String, Long] = data.docIds.map(_.swap)
+
+  /**
+    * Finds the product of a dense matrix and a diagonal matrix represented by a vector.
+    * Breeze doesn't support efficient diagonal representations, so multiply manually.
+    */
+  def multiplyByDiagonalMatrix(mat: Matrix, diag: MLLibVector): BDenseMatrix[Double] = {
+    val sArr = diag.toArray
+    new BDenseMatrix[Double](mat.numRows, mat.numCols, mat.toArray)
+      .mapPairs { case ((r, c), v) => v * sArr(c) }
+  }
+
+  /**
+    * Finds the product of a distributed matrix and a diagonal matrix represented by a vector.
+    */
+  def multiplyByDiagonalRowMatrix(mat: RowMatrix, diag: MLLibVector): RowMatrix = {
+    val sArr = diag.toArray
+    new RowMatrix(mat.rows.map { vec =>
+      val vecArr = vec.toArray
+      val newArr = (0 until vec.size).toArray.map(i => vecArr(i) * sArr(i))
+      Vectors.dense(newArr)
+    })
+  }
+
+  /**
+    * Returns a matrix where each row is divided by its length.
+    */
+  def rowsNormalized(mat: BDenseMatrix[Double]): BDenseMatrix[Double] = {
+    val newMat = new BDenseMatrix[Double](mat.rows, mat.cols)
+    for (r <- 0 until mat.rows) {
+      val length = math.sqrt((0 until mat.cols).map(c => mat(r, c) * mat(r, c)).sum)
+      (0 until mat.cols).foreach(c => newMat.update(r, c, mat(r, c) / length))
+    }
+    newMat
+  }
+
+  /**
+    * Returns a distributed matrix where each row is divided by its length.
+    */
+  def distributedRowsNormalized(mat: RowMatrix): RowMatrix = {
+    new RowMatrix(mat.rows.map { vec =>
+      val array = vec.toArray
+      val length = math.sqrt(array.map(x => x * x).sum)
+      Vectors.dense(array.map(_ / length))
+    })
+  }
+
+  /**
+    * Finds docs relevant to a term. Returns the doc IDs and scores for the docs with the highest
+    * relevance scores to the given term.
+    */
+  def topDocsForTerm(termId: Int): Seq[(Double, Long)] = {
+    val rowArr = (0 until model.V.numCols).map(i => model.V(termId, i)).toArray
+    val rowVec = Matrices.dense(rowArr.length, 1, rowArr)
+
+    // Compute scores against every doc
+    val docScores = US.multiply(rowVec)
+
+    // Find the docs with the highest scores
+    val allDocWeights = docScores.rows.map(_.toArray(0)).zipWithUniqueId
+    allDocWeights.top(10)
+  }
+
+  /**
+    * Finds terms relevant to a term. Returns the term IDs and scores for the terms with the highest
+    * relevance scores to the given term.
+    */
+  def topTermsForTerm(termId: Int): Seq[(Double, Int)] = {
+    // Look up the row in VS corresponding to the given term ID.
+    val rowVec = normalizedVS(termId, ::).t
+
+    // Compute scores against every term
+    val termScores = (normalizedVS * rowVec).toArray.zipWithIndex
+
+    // Find the terms with the highest scores
+    termScores.sortBy(-_._1).take(10)
+  }
+
+  /**
+    * Finds docs relevant to a doc. Returns the doc IDs and scores for the docs with the highest
+    * relevance scores to the given doc.
+    */
+  def topDocsForDoc(docId: Long): Seq[(Double, Long)] = {
+    // Look up the row in US corresponding to the given doc ID.
+    val docRowArr = normalizedUS.rows.zipWithUniqueId.map(_.swap).lookup(docId).head.toArray
+    val docRowVec = Matrices.dense(docRowArr.length, 1, docRowArr)
+
+    // Compute scores against every doc
+    val docScores = normalizedUS.multiply(docRowVec)
+
+    // Find the docs with the highest scores
+    val allDocWeights = docScores.rows.map(_.toArray(0)).zipWithUniqueId
+
+    // Docs can end up with NaN score if their row in U is all zeros.  Filter these out.
+    allDocWeights.filter(!_._1.isNaN).top(10)
+  }
+
+  /**
+    * Builds a term query vector from a set of terms.
+    */
+  def termsToQueryVector(terms: Seq[String]): BSparseVector[Double] = {
+    val indices = terms.map(idTerms(_)).toArray
+    val values = indices.map(data.tfIdfs(_))
+    new BSparseVector[Double](indices, values, idTerms.size)
+  }
+
+  /**
+    * Finds docs relevant to a term query, represented as a vector with non-zero weights for the
+    * terms in the query.
+    */
+  def topDocsForTermQuery(query: BSparseVector[Double]): Seq[(Double, Long)] = {
+    val breezeV = new BDenseMatrix[Double](model.V.numRows, model.V.numCols, model.V.toArray)
+    val termRowArr = (breezeV.t * query).toArray
+
+    val termRowVec = Matrices.dense(termRowArr.length, 1, termRowArr)
+
+    // Compute scores against every doc
+    val docScores = US.multiply(termRowVec)
+
+    // Find the docs with the highest scores
+    val allDocWeights = docScores.rows.map(_.toArray(0)).zipWithUniqueId
+    allDocWeights.top(10)
+  }
+
+  def printTopTermsForTerm(term: String): Unit = {
+    val idWeights = topTermsForTerm(idTerms(term))
+    println(idWeights.map { case (score, id) => (data.termIds(id), score) }.mkString(", "))
+  }
+
+  def printTopDocsForDoc(doc: String): Unit = {
+    val idWeights = topDocsForDoc(idDocs(doc))
+    println(idWeights.map { case (score, id) => (data.docIds(id), score) }.mkString(", "))
+  }
+
+  def printTopDocsForTerm(term: String): Unit = {
+    val idWeights = topDocsForTerm(idTerms(term))
+    println(idWeights.map { case (score, id) => (data.docIds(id), score) }.mkString(", "))
+  }
+
+  def printTopDocsForTermQuery(terms: Seq[String]): Unit = {
+    val queryVec = termsToQueryVector(terms)
+    val idWeights = topDocsForTermQuery(queryVec)
+    println(idWeights.map { case (score, id) => (data.docIds(id), score) }.mkString(", "))
+  }
